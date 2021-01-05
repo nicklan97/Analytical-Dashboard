86088bc0572aa5fbde86da26ba2abdf7af2a9342
nicklan97 <66889918+nicklan97@users.noreply.github.com>
2021-01-05 02:11:36 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+import json
+import plotly
+import plotly.graph_objs as go
 app = Flask(__name__)
 
 
@@ -14,6 +17,17 @@ def search_job(skill):
     data = get_df()
     output, total, skill, ratio = search_job_from_skill(skill, data)
     return render_template('template1.html', output=output, total=total, skill=skill, ratio=ratio)
+
+
+@app.route('/skill-to-industry/<skill>')
+def bar_plot(skill):
+    from search_job import get_df, get_industry
+    df = get_df()
+    output = get_industry(skill, df)
+    data = [go.Bar(x=output['industry'], y=output['counts'])]
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('template4.html',
+                           graphJSON=graphJSON, skill=skill)
 
 
 @app.route('/job-to-skill/<job>')
